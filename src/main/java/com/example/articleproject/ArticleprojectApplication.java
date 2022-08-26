71084ed62198bf50a6ac6462bc3e355a80e8d9be
ChaJaewook <44600628+ChaJaewook@users.noreply.github.com>
2022-08-27 01:38:41 +0900

    - [스프링 부트 입문 08] 데이터 생성 with JPA

--- src/main/java/com/example/articleproject/ArticleprojectApplication.java
+++ src/main/java/com/example/articleproject/ArticleprojectApplication.java
@@ -16,6 +16,21 @@ public class ArticleprojectApplication {
 		// Layout : 화면배치
 		// Header(사이트 안내), Footer(사이트 정보)
 
+		// CRUD
+
+		// 폼데이터 : HTML 요소인 FORM태그에 담긴 데이터 요소 <form>(어디로, 어떻게 보낼지를 기재)
+		// 해당 데이터를 Controller 객체가 받고 그객체를 DTO라고 한다.
+
+		// View Page의 어디로 보낼지 action과 어떻게 보낼지 method로 구분
+		// dto객체에 담기게한다.
+
+		// DB는 JAVA를 모른다. DB는 SQL을 사용한다.
+		// 어떻게 JAVA를 DB가 알아들을 수 있게 해주는게 JPA, 데이터 관리에 필요한 기능도 제공
+		// Entity(DB가 이해 할 수 있는 규격화된 데이터)와 Repository(통해서 DB에 전달)가 있다.
+
+		// DTO를 Entitiy로 변환하고 Repository를 통해 DB에 저장
+
+
 
 
 	}
