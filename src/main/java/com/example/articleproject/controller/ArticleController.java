c56c2283043f109d9c88127f2269731a8659de98
ChaJaewook <44600628+ChaJaewook@users.noreply.github.com>
2022-12-14 00:36:14 +0900

    - [스프링 부트 입문 11] 데이터 조회하기 with JPA

--- src/main/java/com/example/articleproject/controller/ArticleController.java
+++ src/main/java/com/example/articleproject/controller/ArticleController.java
@@ -7,8 +7,12 @@ import com.example.articleproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 @Slf4j // 로깅을 위한 어노테이션
@@ -46,5 +50,24 @@ public class ArticleController {
         //System.out.println(saved.toString());
         log.info(saved.toString());
         return "";
+    }
+
+    //{id}처리를 통해 id의 값은 변할 수 있다.
+    //@PathVariable을 통해 path값의 {id}의 값을 Long id로 받아온다.
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model)
+    {
+        log.info("id="+id);
+
+        // 1) id를 이용해 데이터 가져오기
+        //repository를 통해 가져오기
+        Article articleEntity=articleRepository.findById(id).orElse(null);
+        //id값을 찾았는데 없으면 null을 반환해라 ==> orElse
+
+        // 2) 가져온 데이터를 모델에 입력
+        model.addAttribute("article",articleEntity);
+
+        // 3) 보여줄 페이지를 설정
+        return"articles/show";
     }
 }
